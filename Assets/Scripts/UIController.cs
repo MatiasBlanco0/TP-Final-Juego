24da566006b908428c59d245bb178fb73830142d
MatiasBlanco0 <101400282+MatiasBlanco0@users.noreply.github.com>
2022-06-01 15:41:37 -0300

    Create UIController.cs

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    Text txtSpeed;
+    Text txtTimer;
+    public int resolucion;
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtSpeed = GetComponentInChildren<Text>();
+        txtTimer = GetComponentInChildren<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        txtSpeed.text = ((Mathf.Floor(rb.velocity.magnitude * resolucion) / resolucion)).ToString();
+        //transform.eulerAngles = new Vector3((Mathf.PingPong(0.5f, 180f)) - 90f, 0f, 0f);
+    }
+}
