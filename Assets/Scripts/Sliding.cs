e0b45329c85ad761b7b4fbefd794480512f7713d
MatiasBlanco0 <101400282+MatiasBlanco0@users.noreply.github.com>
2022-06-03 12:03:32 -0300

    Update Sliding.cs

--- Assets/Scripts/Sliding.cs
+++ Assets/Scripts/Sliding.cs
@@ -11,17 +11,15 @@ public class Sliding : MonoBehaviour
     PlayerController playerController;
 
     [Header("Sliding")]
-    public float maxSlideTimer;
+    public float maxSlideTime;
     public float slideForce;
     float slideTimer;
 
     public float slideYScale;
     float startYScale;
 
-    public float horizontalInput;
-    public float verticalInput;
-
-    bool sliding;
+    float horizontalInput;
+    float verticalInput;
 
     // Start is called before the first frame update
     void Start()
@@ -30,6 +28,10 @@ public class Sliding : MonoBehaviour
         playerController = GetComponent<PlayerController>();
 
         startYScale = playerObj.localScale.y;
+
+        maxSlideTime = 0.75f;
+        slideForce = 200;
+        slideYScale = 0.5f;
     }
 
     // Update is called once per frame
@@ -38,31 +40,60 @@ public class Sliding : MonoBehaviour
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
 
-        if (Input.GetKeyDown(KeyCode.LeftControl) && horizontalInput != 0 || verticalInput != 0)
+        if (Input.GetKeyDown(KeyCode.LeftControl) && (horizontalInput != 0 || verticalInput != 0))
         {
             StartSlide();
         }
 
-        if(Input.GetKeyUp(KeyCode.LeftControl) && sliding)
+        if(Input.GetKeyUp(KeyCode.LeftControl) && playerController.sliding)
         {
             StopSlide();
         }
     }
 
+    void FixedUpdate()
+    {
+        if (playerController.sliding)
+        {
+            SlidingMovement();
+        }
+    }
+
     void StartSlide()
     {
-        sliding = true;
+        playerController.sliding = true;
 
         playerObj.localScale = new Vector3(playerObj.localScale.x, slideYScale, playerObj.localScale.z);
+        rb.AddForce(Vector3.down * 5f, ForceMode.Impulse);
+
+        slideTimer = maxSlideTime;
     }
 
     void SlidingMovement()
     {
+        Vector3 inputDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
+        if(!playerController.OnSlope() || rb.velocity.y > -0.1f)
+        {
+            rb.AddForce(inputDirection.normalized * slideForce, ForceMode.Force);
+
+            slideTimer -= Time.deltaTime;
+        }
+        else
+        {
+            rb.AddForce(playerController.GetSlopeMoveDirection(inputDirection) * slideForce, ForceMode.Force);
+        }
+
+        if (slideTimer <= 0)
+        {
+            StopSlide();
+        }
     }
 
     void StopSlide()
     {
+        playerController.sliding = false;
 
+        playerObj.localScale = new Vector3(playerObj.localScale.x, startYScale, playerObj.localScale.z);
     }
 }
