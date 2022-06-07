2727e09a82cd5418b104c5c8031b87c012ef1c3d
MatiasBlanco0 <101400282+MatiasBlanco0@users.noreply.github.com>
2022-06-07 10:34:36 -0300

    Update WallRunning.cs

--- Assets/Scripts/WallRunning.cs
+++ Assets/Scripts/WallRunning.cs
@@ -17,16 +17,64 @@ public class WallRunning : MonoBehaviour
     [Header("Detection")]
     public float wallCheckDistance;
     public float minJumpHeight;
+    RaycastHit leftWallHit;
+    RaycastHit rightWallHit;
+    bool wallLeft;
+    bool wallRight;
+
+    [Header("References")]
+    public Transform orientation;
+    PlayerController playerController;
+    Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        playerController = GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CheckForWall();
+
+    }
+
+    void CheckForWall()
+    {
+        wallRight = Physics.Raycast(transform.position, orientation.right, out rightWallHit, wallCheckDistance, whatIsWall);
+        wallLeft = Physics.Raycast(transform.position, -orientation.right, out leftWallHit, wallCheckDistance, whatIsWall);
+    }
+
+    bool AboveGround()
+    {
+        return !Physics.Raycast(transform.position, Vector3.down, minJumpHeight, whatIsGround);
+    }
+
+    void StateMachine()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        if((wallLeft || wallRight) && verticalInput > 0 && AboveGround())
+        {
+
+        }
+    }
+
+    void StartWallRun()
+    {
+
+    }
+
+    void WallRunningMovement()
+    {
+
+    }
+
+    void StopWallRun()
+    {
+
     }
 }
