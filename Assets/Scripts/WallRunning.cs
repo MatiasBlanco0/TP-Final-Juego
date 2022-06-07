edb76db069f2e60ad86b6a538c5734e2dccd0ca0
MatiasBlanco0 <101400282+MatiasBlanco0@users.noreply.github.com>
2022-06-07 10:22:04 -0300

    Create WallRunning.cs

---
+++ Assets/Scripts/WallRunning.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallRunning : MonoBehaviour
+{
+    [Header("Wall Running")]
+    public LayerMask whatIsWall;
+    public LayerMask whatIsGround;
+    public float wallRunForce;
+    public float maxWallRunTime;
+    float wallRunTimer;
+
+    float horizontalInput;
+    float verticalInput;
+
+    [Header("Detection")]
+    public float wallCheckDistance;
+    public float minJumpHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
