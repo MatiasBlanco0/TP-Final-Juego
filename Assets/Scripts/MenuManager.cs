0eb3d1a69649831e53b39acee99197307d334d46
MatiasBlanco0 <101400282+MatiasBlanco0@users.noreply.github.com>
2022-06-10 11:44:25 -0300

    Create MenuManager.cs

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject levelSelector;
+    public GameObject menu;
+
+    void Start()
+    {
+        levelSelector.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Play()
+    {
+        levelSelector.SetActive(true);
+        menu.SetActive(false);
+    }
+
+    public void Back()
+    {
+        menu.SetActive(true);
+        levelSelector.SetActive(false);
+    }
+
+    public void LoadLevel(string lvl)
+    {
+        SceneManager.LoadScene(lvl);
+    }
+}
