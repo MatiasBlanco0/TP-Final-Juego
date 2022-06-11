7afdc9100546769de81c64a86cbb80fa4df42445
MatiasBlanco0 <101400282+MatiasBlanco0@users.noreply.github.com>
2022-06-11 20:13:21 -0300

    Update CameraController.cs

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -30,6 +30,6 @@ public class CameraController : MonoBehaviour
         xRotation = Mathf.Clamp(xRotation, -90f,90f);
 
        camHolder.rotation = Quaternion.Euler(xRotation,yRotation,0);
-        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+       orientation.rotation = Quaternion.Euler(0, yRotation, 0);
     }
 }
