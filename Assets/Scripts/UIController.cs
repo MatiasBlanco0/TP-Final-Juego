1a85ae3d7ab0164173dbd9038fb438270845b58f
MatiasBlanco0 <101400282+MatiasBlanco0@users.noreply.github.com>
2022-06-12 15:54:21 -0300

    Update UIController.cs

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -5,25 +5,48 @@ using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
-    public GameObject countdown;
+    public GameObject countdownObj;
+    public Text txtCountdown;
+    public float countdown = 3f;
+
     public GameObject hud;
     public Text txtSpeed;
+    float timeElapssed = 0;
     public Text txtTimer;
-    public int resolucion;
+
+    public int resolution;
     public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-
+        hud.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Multiplico la velocidad por la resolucion, hago floor y despues lo divido por la resolucion,
-        // eliminando algunos numeros decimales que no son necesarios
-        txtSpeed.text = ((Mathf.Floor(rb.velocity.magnitude * resolucion) / resolucion)).ToString();
+        if (hud.activeInHierarchy)
+        {
+            // Multiplico la velocidad por la resolucion, hago floor y despues lo divido por la resolucion,
+            // eliminando algunos numeros decimales que no son necesarios
+            txtSpeed.text = (Mathf.Floor(rb.velocity.magnitude * resolution) / resolution).ToString();
 
-        txtTimer.text = (Mathf.Floor(Time.time)).ToString();
+            txtTimer.text = Mathf.Floor(timeElapssed).ToString();
+
+            timeElapssed += Time.deltaTime;
+        }
+
+        if(countdown <= 0)
+        {
+            hud.SetActive(true);
+            countdownObj.SetActive(false);
+        }
+
+        if (countdownObj.activeInHierarchy)
+        {
+            countdown -= Time.deltaTime;
+
+            txtCountdown.text = Mathf.Floor(countdown).ToString();
+        }
     }
 }
