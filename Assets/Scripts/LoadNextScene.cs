9dfdb7bf3223a0402dec93e04ac611a2a57f79d2
MatiasBlanco0 <101400282+MatiasBlanco0@users.noreply.github.com>
2022-06-13 09:34:36 -0300

    Update LoadNextScene.cs

--- Assets/Scripts/LoadNextScene.cs
+++ Assets/Scripts/LoadNextScene.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadNextScene : MonoBehaviour
 {
+    public PlayerController playerController;
+    float timer = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,25 @@ public class LoadNextScene : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerController.won)
+        {
+            if(timer <= 0)
+            {
+                string sceneName = SceneManager.GetActiveScene().name;
+                if(sceneName == "Sandbox")
+                {
+                    SceneManager.LoadScene("Menu");
+                }
+                int lvlNum = int.Parse(sceneName[-1].ToString());
+
+                string nextLVLName = sceneName.Substring(0, 5) + lvlNum.ToString();
+
+                SceneManager.LoadScene(nextLVLName);
+            }
+            else
+            {
+                timer -= Time.deltaTime;
+            }
+        }
     }
 }
