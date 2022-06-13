0483ff9a25a96d3b5667b6c87add6e20e16d4355
MatiasBlanco0 <101400282+MatiasBlanco0@users.noreply.github.com>
2022-06-13 20:09:03 -0300

    Update LoadNextScene.cs

--- Assets/Scripts/LoadNextScene.cs
+++ Assets/Scripts/LoadNextScene.cs
@@ -5,13 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class LoadNextScene : MonoBehaviour
 {
+    public GameObject winScreen;
+    public GameObject btnNextLVL;
+
     public PlayerController playerController;
     float timer = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        winScreen.SetActive(false);
     }
 
     // Update is called once per frame
@@ -19,27 +22,31 @@ public class LoadNextScene : MonoBehaviour
     {
         if (playerController.won)
         {
-            if(timer <= 0)
-            {
-                string sceneName = SceneManager.GetActiveScene().name;
-                Cursor.lockState = CursorLockMode.None;
-                if (sceneName == "Sandbox")
-                {
-                    SceneManager.LoadScene("Menu");
-                }
-                else
-                {
-                    int lvlNum = int.Parse(sceneName[6].ToString());
-
-                    string nextLVLName = sceneName.Substring(0, 5) + lvlNum.ToString();
-
-                    SceneManager.LoadScene(nextLVLName);
-                }
-            }
-            else
-            {
-                timer -= Time.deltaTime;
-            }
+            winScreen.SetActive(true);
         }
+    }
+
+    public void LoadNextLevel()
+    {
+        string sceneName = SceneManager.GetActiveScene().name;
+        Cursor.lockState = CursorLockMode.None;
+
+        if(sceneName == "Sandbox")
+        {
+            btnNextLVL.SetActive(false);
+        }
+        else
+        {
+            int lvlNum = int.Parse(sceneName[6].ToString());
+
+            string nextLVLName = sceneName.Substring(0, 5) + lvlNum.ToString();
+
+            SceneManager.LoadScene(nextLVLName);
+        }
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
     }
 }
