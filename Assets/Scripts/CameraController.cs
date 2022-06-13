e467d387e2efebbb83d8c37dc057adb5c47ba9c3
MatiasBlanco0 <101400282+MatiasBlanco0@users.noreply.github.com>
2022-06-13 09:19:00 -0300

    Update CameraController.cs

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -10,6 +10,8 @@ public class CameraController : MonoBehaviour
     public Transform orientation;
     public Transform camHolder;
 
+    public PlayerController playerController;
+
     float xRotation;
     float yRotation;
     // Start is called before the first frame update
@@ -21,15 +23,18 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
-        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+        if (!playerController.won)
+        {
+            float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+            float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
 
-        yRotation += mouseX;
+            yRotation += mouseX;
 
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f,90f);
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-       camHolder.rotation = Quaternion.Euler(xRotation,yRotation,0);
-       orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+            camHolder.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+            orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+        }
     }
 }
